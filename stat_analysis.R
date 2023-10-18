9fd7a3b66db6bd77c3056631766408f30bd3b5f4
val <valeriyadontsova@gmail.com>
2023-10-18 13:57:18 +0300

    Normality check by SW test functions completed in stat analysis file

--- stat_analysis.R
+++ stat_analysis.R
@@ -7,24 +7,22 @@
 # number of unique parameters i.e time, treatment etc
 
 # shapiro wilk test for normality
-normality.sw <- function(data_table, norm_vector_name, p.value = 0.05){
-    # Uses data_table as input for analysis, stratifying based on the unique values
-    # in each column specified in parmtrs. norm_vector_name indicates the column of values
+normality.sw <- function(norm_vector, p.value = 0.05){
+    # Uses norm_vector as input for the Shapiro-Wilk test for normality of data.
+    # norm_vector: vector of values
+    # p.value indicates the probability threshold for assigning normality to a given distribution
+    # The function returns a vector of p-value, test-statistic, and normality boolean
+
+    sw.test <- shapiro.test(norm_vector)
+    return(c(sw.test$p.value, sw.test$statistic, sw.test$p.value > p.value))
+}
+
+rec.sw.test <- function(data_table, parmtrs, norm_vector_name, p.value = 0.05){
+    # Uses data_table as input for analysis, stratifying based on the unique values in each column specified in parmtrs. norm_vector_name indicates the column of values
     # for the Shapiro-Wilk test for normality of data.
     # data_table: data frame
     # p.value indicates the probability threshold for assigning normality to a given distribution
-    # The function returns a table of values
-    
-    return_table <- data.frame()
-    st <- shapiro.test(data_table[,norm_vector_name])
-    return_table <- rbind(return_table, data_frame(pvalue = st$p.value, stat = st$statistic))
-    return(return_table)
-
-}
-
-rec.sw.test <- function(data_table, parmtrs, norm_vector_name){
-    # Returns data frame with selected rows
-    # Can be used for recursion
+    # Recursively determines every combination of unique vectors for SW test based on specified columns in parmtrs
     
     return_table <- data.frame()
 
@@ -33,17 +31,25 @@ rec.sw.test <- function(data_table, parmtrs, norm_vector_name){
     }
     else if(length(parmtrs) == 1){
         for(i in unique(data_table[,parmtrs])){
-            sel_vector <- data_table[data_table[,parmtrs] == i,]
-            return_table <- rbind(return_table, normality.sw())
+            norm_stat <- normality.sw(data_table[data_table[,parmtrs] == i, norm_vector_name], p.value)
+            return_table_row <- data.frame(a = i, b = norm_stat[1], c  = norm_stat[2], d = norm_stat[3])
+            colnames(return_table_row) <- c(parmtrs, 'p.value', 'test.statistic', 'normal.distribution')
+            return_table <- rbind(return_table, return_table_row)
         }
         return(return_table)
     }
     else{
         for(i in unique(data_table[,parmtrs[1]])){
-            # call itself
-            sel_vector <- data_table[data_table[,parmtrs[1]] == i,]
-            return_table <- rbind(return_table, rec.sw.test(sel_vector, parmtrs[-1]))
+        
+           # call itself
+            self_table <- rec.sw.test(data_table[data_table[,parmtrs[1]] == i,], parmtrs[-1], norm_vector_name)
+            
+            return_table_row <- data.frame(a = i)
+            colnames(return_table_row) <- c(parmtrs[1])
+            
+            return_table_row <- cbind(return_table_row, self_table)
             # add to return table
+            return_table <- rbind(return_table, return_table_row)
         }
         return(return_table)
     }
